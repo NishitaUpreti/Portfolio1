2fa0843c2ba77c2efdeea3c81a5279e5d917acd8
Nishita Upreti <nishitaupreti@Nishitas-MacBook-Air.local>
2025-01-25 23:35:15 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,10 @@
+function redirectToWebsite1(){
+    window.location.href = "https://github.com/NishitaUpreti";
+}
+function redirectToWebsite2(){
+    window.location.href = "https://mail.google.com/mail/u/0/#inbox?compose=CllgCJNxNSMncpFXjmgpqdSnFHRKWsDzwccgpSBXQpdVXkfZjKXwHLPVsSMNjmqWcXDGWckbQCg";
+}
+function redirectToWebsite3(){
+    window.location.href = "http://www.linkedin.com/in/nisihta-upreti";
+}
+
